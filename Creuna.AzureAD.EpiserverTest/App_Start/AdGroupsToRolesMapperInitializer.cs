fd20eea3b8c3f8a4342721455eb74a351ba4b02b
Roman Mironets <roman.mironets@creuna.no>
2018-01-22 10:38:52 +0200

    Creuna.AzureAD is switched to EPiServer 11, removed structuremap dependency, registry implementation moved to readme file, test project is updated accordingly.

---
+++ Creuna.AzureAD.EpiserverTest/App_Start/AdGroupsToRolesMapperInitializer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Creuna.AzureAD.Configuration;
+using Creuna.AzureAD.Configuration.ConfigFile;
+using Creuna.AzureAD.Configuration.Episerver;
+using Creuna.AzureAD.Contracts;
+using Creuna.AzureAD.Utils.FeatureToggles;
+using EPiServer.Framework;
+using EPiServer.ServiceLocation;
+using StructureMap;
+
+namespace Creuna.AzureAD.EpiserverTest
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(ServiceContainerInitialization))]
+    public class AdGroupsToRolesMapperInitializer : IConfigurableModule
+    {
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
+            var container = context.StructureMap();
+
+            container.Configure(x => x.AddRegistry<AdGroupsToRolesMapperRegistry>());
+        }
+
+        public void Initialize(EPiServer.Framework.Initialization.InitializationEngine context)
+        { }
+
+        public void Preload(string[] parameters)
+        { }
+
+        public void Uninitialize(EPiServer.Framework.Initialization.InitializationEngine context)
+        { }
+    }
+
+    public class AdGroupsToRolesMapperRegistry : Registry
+    {
+        public AdGroupsToRolesMapperRegistry()
+        {
+            if (!new FeatureToggleDisableAzureAD().FeatureEnabled)
+            {
+
+                if (new FeatureToggleForseJsonConfig().FeatureEnabled)
+                {
+                    JsonConfigAdGroupsToRolesMapper();
+                }
+                else
+                {
+                    EpiserverAdGroupsToRolesMapper();
+                }
+            }
+        }
+
+        private void JsonConfigAdGroupsToRolesMapper()
+        {
+            CommonComponents();
+
+            For<AzureAdSecurityConfigurationFileProvider>().Singleton();
+            For<IAzureAdSecuritySettingsProvider>().Singleton().Use(ctx => ctx.GetInstance<AzureAdSecurityConfigurationFileProvider>());
+            For<ICustomVirtualRolesProvider>().Singleton().Use(ctx => ctx.GetInstance<AzureAdSecurityConfigurationFileProvider>());
+        }
+
+        private void EpiserverAdGroupsToRolesMapper()
+        {
+            CommonComponents();
+
+            For<AzureAdSecurityConfigurationFileProvider>().Singleton();
+            For<AzureAdSecurityEpiserverProvider>().Singleton().Use<AzureAdSecurityEpiserverProvider>()
+                .Ctor<IAzureAdSecuritySettingsProvider>().Is<AzureAdSecurityConfigurationFileProvider>();
+            For<IAzureAdSecuritySettingsProvider>().Singleton().Use(ctx => ctx.GetInstance<AzureAdSecurityEpiserverProvider>());
+            For<ICustomVirtualRolesProvider>().Singleton().Use(ctx => ctx.GetInstance<AzureAdSecurityEpiserverProvider>());
+        }
+
+        private void CommonComponents()
+        {
+            For<IIdentityUpdater>().Singleton().Use<AdGroupsToRolesIdentityUpdater>();
+            For<ICustomVirtualRolesWatcher>().Singleton().Use<RolesWatcher>();
+        }
+    }
+}
--- Creuna.AzureAD.EpiserverTest/modules/_protected/Creuna.AzureAD/readme.txt
+++ Creuna.AzureAD.EpiserverTest/modules/_protected/Creuna.AzureAD/readme.txt
@@ -7,7 +7,9 @@ Creuna.AzureAD Quickstart
 	- Creuna.AzureAD.Tenant
 	- Creuna.AzureAD.AADInstance
 	- Creuna.AzureAD.PostLogoutRedirectUri
-
+Update reply URLs to include $(site) $(site)/episerver/ $(site)/episerver/cms/ for each episerver site 
+(i.e. http://mysite.local/episerver/, http://mysite.local/episerver/cms/) 
+Unfortunately reply urls are case sensitive, so $(site)/EPiServer/ and $(site)/EPiServer/CMS/ should also be added
 
 NOTE: the following 2 steps are not needed if you have an Azure Premium subscription and manage roles in the Azure Portal
 3. update your app manifest to have 
@@ -41,6 +43,51 @@ NOTE: the following 2 steps are not needed if you have an Azure Premium subscrip
 
         public void Uninitialize(EPiServer.Framework.Initialization.InitializationEngine context)
         { }
+    }
+
+    public class AdGroupsToRolesMapperRegistry : Registry
+    {
+        public AdGroupsToRolesMapperRegistry()
+        {
+            if (!new FeatureToggleDisableAzureAD().FeatureEnabled)
+            {
+
+                if (new FeatureToggleForseJsonConfig().FeatureEnabled)
+                {
+                    JsonConfigAdGroupsToRolesMapper();
+                }
+                else
+                {
+                    EpiserverAdGroupsToRolesMapper();
+                }
+            }
+        }
+
+        private void JsonConfigAdGroupsToRolesMapper()
+        {
+            CommonComponents();
+
+            For<AzureAdSecurityConfigurationFileProvider>().Singleton();
+            For<IAzureAdSecuritySettingsProvider>().Singleton().Use(ctx => ctx.GetInstance<AzureAdSecurityConfigurationFileProvider>());
+            For<ICustomVirtualRolesProvider>().Singleton().Use(ctx => ctx.GetInstance<AzureAdSecurityConfigurationFileProvider>());
+        }
+
+        private void EpiserverAdGroupsToRolesMapper()
+        {
+            CommonComponents();
+
+            For<AzureAdSecurityConfigurationFileProvider>().Singleton();
+            For<AzureAdSecurityEpiserverProvider>().Singleton().Use<AzureAdSecurityEpiserverProvider>()
+                .Ctor<IAzureAdSecuritySettingsProvider>().Is<AzureAdSecurityConfigurationFileProvider>();
+            For<IAzureAdSecuritySettingsProvider>().Singleton().Use(ctx => ctx.GetInstance<AzureAdSecurityEpiserverProvider>());
+            For<ICustomVirtualRolesProvider>().Singleton().Use(ctx => ctx.GetInstance<AzureAdSecurityEpiserverProvider>());
+        }
+
+        private void CommonComponents()
+        {
+            For<IIdentityUpdater>().Singleton().Use<AdGroupsToRolesIdentityUpdater>();
+            For<ICustomVirtualRolesWatcher>().Singleton().Use<RolesWatcher>();
+        }
     }
 
 6. Make sure authentication, roleManager and membership are turned off for AzureAD
